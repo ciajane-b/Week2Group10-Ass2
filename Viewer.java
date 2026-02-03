c61c46a4acee2c035fcc174d780849d22c5663ef
santinoarevalo66-hash <santinoarevalo66@gmail.com>
2026-02-03 16:04:03 +0800

    Update Viewer.java

--- Viewer.java
+++ Viewer.java
@@ -1,5 +1,15 @@
 class Viewer {
+    
     public int viewerId;
     public String name;
-  
+
+     public Viewer(int viewerId, String name) {
+        this.viewerId = viewerId;
+        this.name = name;
+    }
+    
+     public String identify() {
+        return "Viewer ID: " + viewerId + ", Name: " + name;
+         
+    }
 }
