023d40db5041c87b2dc9fa4e04cceaaf36c59e28
ciajane-b <ciarajanebarro22@gmail.com>
2026-02-03 15:50:29 +0800

    Add methods to calculate total and identify ticket

--- Ticket.java
+++ Ticket.java
@@ -3,13 +3,20 @@ class Ticket {
 
     // Aggregation
     public Viewer viewer;
-
     // Composition
     public Movie movie;
-
+    
     public Ticket(int ticketId, Viewer viewer, Movie movie) {
         this.ticketId = ticketId;
         this.viewer = viewer;
         this.movie = movie;
+    }
+    
+    public double calculateTotal() {
+        return movie.getPrice();
+    }
+
+    public String identify() {
+        return "Ticket ID: " + ticketId;
     }
 }
