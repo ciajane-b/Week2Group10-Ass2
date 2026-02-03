5b0639a736739813ac7d071905a20cfce2478ff0
AldCream <aldrianmaure@gmail.com>
2026-02-03 15:40:12 +0800

    Add constructor to Movie class

--- Movie.java
+++ Movie.java
@@ -2,5 +2,12 @@ class Movie {
     public int movieId;
     public String title;
     public double price;
-  
+
+    public Movie(int movieId, String title, double price) {
+        this.movieId = movieId;
+        this.title = title;
+        this.price = price;
+    }
+
+    
 }
